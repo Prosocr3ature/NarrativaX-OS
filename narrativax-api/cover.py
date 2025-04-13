f800f105b62a777e5bcc765979daa9a8fea599ad
Prosocr3ature <35155025+Prosocr3ature@users.noreply.github.com>
2025-04-13 03:03:59 +0200

    Update cover.py

--- narrativax-api/cover.py
+++ narrativax-api/cover.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
 def generate_cover_image(prompt, size="1024x1024"):
     response = openai.images.generate(
