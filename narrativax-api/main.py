63fdc5499ac92389af7261c7b0887d29b74c929d
Prosocr3ature <35155025+Prosocr3ature@users.noreply.github.com>
2025-04-17 16:25:17 +0200

    Update main.py

--- narrativax-api/main.py
+++ narrativax-api/main.py
@@ -117,7 +117,7 @@ def narrate(text, id_key):
 
 # --- SIDEBAR ---
 with st.sidebar:
-    st.image("https://i.imgur.com/vGV9N5k.png", width=180)
+    st.image("public/logo.png", width=180)
     st.markdown("### NarrativaX PWA")
     st.info("Safari → Dela → Lägg till på hemskärmen för att spara som app.")
     if st.button("Save Project"):
